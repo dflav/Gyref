284526cbcfaaf5167d2cd1ac79319cbbda482660
Flavio Dollani <dflav.me@gmail.com>
2019-12-08 20:03:15 +0200

    Working responsive navbar

--- src/components/Humburger.js
+++ src/components/Humburger.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Humburger(props) {
   return (
     <>
-      <input type='checkbox' className='toggler' />
+      <input
+        onClick={() => props.toggle()}
+        type='checkbox'
+        className='toggler'
+      />
       <div className='humburger'>
         <div className='btn-line'></div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Humburger from './Humburger';
 
-function Navbar() {
-  return (
-    <>
-      <div className='navbar'>
-        <img
-          className='logo'
-          src='./images/cover.png'
-          alt='A person lifting weights'
-        />
-        <div className='menu-wrap'>
-          <Humburger />
+class Navbar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      toggle: false
+    };
+  }
+
+  updateNav = () => {
+    this.setState((prevState) => ({
+      toggle: !prevState.toggle
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <div className='navbar'>
+          <img
+            className='logo'
+            src='./images/cover.png'
+            alt='A person lifting weights'
+          />
+          <div className='menu-wrap'>
+            <Humburger toggle={this.updateNav} />
+          </div>
+          <nav className={this.state.toggle ? 'on' : 'off'}>
+            <ul className='nav-links'>
+              <li>
+                <a href='#!'>Home</a>
+              </li>
+              <li>
+                <a href='#!'>Workouts</a>
+              </li>
+              <li>
+                <a href='#!'>Exercises</a>
+              </li>
+            </ul>
+          </nav>
+          <a className='login-btn' href='#!'>
+            <button>Login</button>
+          </a>
         </div>
-        <nav>
-          <ul className='nav-links'>
-            <li>
-              <a href='#!'>Home</a>
-            </li>
-            <li>
-              <a href='#!'>Workouts</a>
-            </li>
-            <li>
-              <a href='#!'>Exercises</a>
-            </li>
-          </ul>
-        </nav>
-        <a className='login-btn' href='#!'>
-          <button>Login</button>
-        </a>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Navbar;
