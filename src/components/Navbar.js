9568fa77b5f3345058df7ff76ee43f99b7b49443
Flavio Dollani <dflav.me@gmail.com>
2019-12-12 15:14:10 +0200

    Navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Navbar() {
-  return (
-    <div className='navbar' /*style={{this.state.animation}}*/>
-      <nav>
-        <ul className='menu'>
-          <li className='logo'>
-            <a href='#!'>
-              <img
-                className='logo'
-                src='./images/cover.png'
-                alt='Gyref background'
+class Navbar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      animation: false
+    };
+  }
+
+  handleChange = () => {
+    this.setState((prevState) => {
+      return { animation: !prevState.animation };
+    });
+  };
+  render() {
+    const animation = { maxHeight: this.state.animation ? '410px' : '100px' };
+
+    return (
+      <div className='navbar' /*style={{this.state.animation}}*/>
+        <nav style={animation}>
+          <ul className='menu'>
+            <li className='logo'>
+              <a href='#!'>
+                <img
+                  className='logo'
+                  src='./images/cover.png'
+                  alt='Gyref background'
+                />
+              </a>
+            </li>
+            <li className='item '>
+              <a href='#!'>Home</a>
+            </li>
+            <li className='item'>
+              <a href='#!'>Workouts</a>
+            </li>
+            <li className='item'>
+              <a href='#!'>Exercises</a>
+            </li>
+            <li className='item button'>
+              <a href='#!'>Login</a>
+            </li>
+            <li className='item button secondary'>
+              <a href='#!'>Sing Up</a>
+            </li>
+            <li className='toggle'>
+              <input
+                onClick={this.handleChange}
+                type='checkbox'
+                className='toggler'
               />
-            </a>
-          </li>
-          <li className='item '>
-            <a href='#!'>Home</a>
-          </li>
-          <li className='item'>
-            <a href='#!'>Workouts</a>
-          </li>
-          <li className='item'>
-            <a href='#!'>Exercises</a>
-          </li>
-          <li className='item button'>
-            <a href='#!'>Login</a>
-          </li>
-          <li className='item button secondary'>
-            <a href='#!'>Sing Up</a>
-          </li>
-          <li className='toggle'>
-            <input type='checkbox' className='toggler' />
-            <span className='bars'></span>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+              <span className='bars'></span>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
