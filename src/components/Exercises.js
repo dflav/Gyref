cbdd470291fbae5904280a0963330e474e56fd95
Flavio Dollani <dflav.me@gmail.com>
2020-02-24 13:19:19 +0200

    Exercise database

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import exersices from '../constants/data';
 class Exercises extends Component {
   constructor() {
     super();
     this.state = {
-      data: null
+      data: [],
+      muscles: true,
+      equipment: false,
+      type: false
     };
   }
+
+  componentDidMount() {
+    this.setState({
+      data: exersices
+    });
+  }
+
+  handleClick = (props) => {
+    if (props === 'muscles') {
+      this.setState({
+        muscles: true,
+        equipment: false,
+        type: false
+      });
+    } else if (props === 'equipment') {
+      this.setState({
+        muscles: false,
+        equipment: true,
+        type: false
+      });
+    } else {
+      this.setState({
+        muscles: false,
+        equipment: false,
+        type: true
+      });
+    }
+  };
 
   render() {
     return (
@@ -20,7 +52,38 @@ class Exercises extends Component {
             </p>
           </div>
         </div>
-        <section></section>
+        <section className='types'>
+          <div>
+            <h4>CHOOSE AN EXERCISE TYPE</h4>
+            <p>
+              Learn about over a 100 exercises that exist in our database by
+              choosing one category below.
+            </p>
+
+            <ul className='ex-nav'>
+              <li
+                className={this.state.muscles ? 'show' : 'hide'}
+                onClick={(e) => this.handleClick('muscles', e)}
+              >
+                <span>Muscles</span>
+              </li>
+              <li
+                className={this.state.equipment ? 'show' : 'hide'}
+                onClick={(e) => this.handleClick('equipment', e)}
+              >
+                <span>Equipment</span>
+              </li>
+              <li
+                className={this.state.type ? 'show' : 'hide'}
+                onClick={(e) => this.handleClick('type', e)}
+              >
+                <span>Type</span>
+              </li>
+            </ul>
+
+            <div className='grid-container'></div>
+          </div>
+        </section>
       </>
     );
   }
--- src/constants/data.js
+++ src/constants/data.js
@@ -1,13 +1,14 @@
-const data = [
+const exersices = [
   {
-    id: 'a',
-    type: 'a',
-    main: 'a',
-    other: 'a',
-    difficulty: 'a',
-    equipment: 'a',
-    image: 'a',
-    description: 'a'
-  },
-  {}
+    id: '1',
+    type: 'Strength',
+    main: 'Chest',
+    other: 'Triceps, Shoulders',
+    difficulty: 'Beginner',
+    equipment: 'Barbell',
+    image: './image/bg.jpg',
+    description: 'blah'
+  }
 ];
+
+export default { exersices };
