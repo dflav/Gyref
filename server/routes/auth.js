f514f4731b41838f006e05214fb3b6b52ddb7158
Flavio Dollani <dflav.me@gmail.com>
2020-03-27 02:15:45 +0200

    Added login validation and authentication with jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { validRegister } = require('../validation');
+const { validRegister, validLogin } = require('../validation');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,27 +11,43 @@ router.post('/register', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   //check if user exists
-  const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).send('User already exists');
+  const user = await User.findOne({ email });
+  if (user) return res.status(400).send('User already exists');
 
   //hash password
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
   //create new user
-  const user = new User({
+  const newUser = new User({
     name,
     email,
     password: hash
   });
   try {
-    const savedUser = await user.save();
+    const savedUser = await newUser.save();
     res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-// router.post('/login', (req, res) => {});
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const { error } = validLogin(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).send('Email or password is wrong');
+
+  const validPass = await bcrypt.compare(password, user.password);
+  if (!validPass) return res.status(400).send('Email or password is wrong');
+
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
+
+  res.send('Logged in!');
+});
 
 module.exports = router;
